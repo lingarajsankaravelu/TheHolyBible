4d639fd8ee4d8e86f60d1d5e65064cd533796608
lingarajsankaravelu <lingaraj.s@hotmail.com>
2015-06-30 14:19:57 +0530

    pinch zoom for reading version 1+

--- app/src/main/java/com/myfirst/lingaraj/theholybible/Amos_main.java
+++ app/src/main/java/com/myfirst/lingaraj/theholybible/Amos_main.java
@@ -6,13 +6,17 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +24,16 @@ import android.widget.Toast;
 /**
  * Created by lingaraj on 6/19/2015.
  */
-public class Amos_main extends Fragment {
+public class Amos_main extends Fragment implements View.OnTouchListener {
+    final static float STEP = 200;
+
+    float mRatio = 1.0f;
+    int mBaseDist;
+    float mBaseRatio;
+    float fontsize = 13;
+    public ScrollView myscrollview;
+
+
     public DatabaseAssetHelper mydb;
     public static final String preferance_name="Bible_Preference";
     public Spinner mySpinner;
@@ -35,10 +48,11 @@ public class Amos_main extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-
         View rootview = inflater.inflate(R.layout.genesis_main, container, false);
         mySpinner = (Spinner) rootview.findViewById(R.id.number_spin);
+        mySpinner.requestDisallowInterceptTouchEvent(true);
         mytextview=(TextView)rootview.findViewById(R.id.mytextview);
+        myscrollview=(ScrollView) rootview.findViewById(R.id.scrollView_genesis);
         mydb=new DatabaseAssetHelper(getActivity());
         myadap = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, mylist);
        setHasOptionsMenu(true);
@@ -49,6 +63,9 @@ public class Amos_main extends Fragment {
 
         myadap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         get_shared_preferencevalue();
+        mytextview.setOnTouchListener(this);
+
+
 
         mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -74,6 +91,8 @@ public class Amos_main extends Fragment {
 
 
     }
+
+
     private void get_shared_preferencevalue() {
 
         try {
@@ -142,7 +161,57 @@ public class Amos_main extends Fragment {
     }
 
 
+    int getDistance(MotionEvent event) {
+        int dx = (int) (event.getX(0) - event.getX(1));
+        int dy = (int) (event.getY(0) - event.getY(1));
+        return (int) (Math.sqrt(dx * dx + dy * dy));
+    }
 
+
+    public boolean onTouch(View v, MotionEvent event) {
+
+
+        if (event.getPointerCount() == 2) {
+
+            int action = event.getAction();
+            int pureaction = action & MotionEvent.ACTION_MASK;
+            if (pureaction == MotionEvent.ACTION_POINTER_DOWN) {
+
+                mBaseDist = getDistance(event);
+                mBaseRatio = mRatio;
+                myscrollview.requestDisallowInterceptTouchEvent(true);
+
+
+
+
+            }
+
+
+            else {
+                float delta = (getDistance(event) - mBaseDist) / STEP;
+                float multi = (float) Math.pow(2, delta);
+                mRatio = Math.min(1024.0f, Math.max(0.1f, mBaseRatio * multi));
+                mytextview.setTextSize(mRatio + 13);
+
+
+
+
+            }
+
+        }
+
+
+
+        return true;
+
+
+
+
+
+    }
 
 
 }
+
+
+
